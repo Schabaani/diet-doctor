a231764d46b9b6c86519532be003bc3d1993d646
Amish <a.shabani@megagroup.biz>
2021-03-05 13:57:07 +0330

    add enums

---
+++ src/utils/enums.ts
@@ -0,0 +1,27 @@
+export const MarginSizes = Object.freeze({
+  XLarge: 20,
+  Large: 15,
+  Medium: 10,
+  Small: 5
+});
+
+export const FontSize = Object.freeze({
+  Small: 10,
+  Medium: 12,
+  Large: 14,
+  XLarge: 16,
+  XXLarge: 20
+});
+
+export const Colors = Object.freeze({
+  Red: 'red',
+  Black: '#000',
+  White: '#fff',
+  GreenDiet: '#14db56',
+  Blue: '#00ff'
+});
+
+export const BorderRadius = Object.freeze({
+  Small: 5,
+  Medium: 10
+});
